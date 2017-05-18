54e2bc2fa2305b7e5e8f27d17bc0948f9832866f
rushi98 <rushikeshjogdand1@gmail.com>
2017-05-18 11:57:35 +0530

    Issue #17: Added firebase support and enabled offline database access

--- app/src/main/java/in/ac/bits_pilani/goa/ard/ARD.java
+++ app/src/main/java/in/ac/bits_pilani/goa/ard/ARD.java
@@ -3,6 +3,8 @@ package in.ac.bits_pilani.goa.ard;
 import android.app.Application;
 import android.content.Context;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Custom class extending the Application class.
  * @author vikramaditya
@@ -13,6 +15,19 @@ public class ARD extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        /*
+            to enable offline firebase database access
+            - only data that is requested/downloaded while user is online is cached
+            - to force download data at specific locations even when it's not used by user while
+                online and may use while offline
+                follow this example-
+                ```
+                DatabaseReference importantData = FirebaseDatabase.getInstance().getReference("impData");
+                importantData.keepSynced(true);
+                ```
+            - related documentation: https://firebase.google.com/docs/database/android/offline-capabilities
+        */
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 
     @Override
